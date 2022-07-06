4b0bbed3cd80e4ea17080ae82326388820f9b9eb
0xATLAS <shiftergamer12@gmail.com>
2022-07-06 20:09:24 +0200

    Add files

---
+++ WinSock_Telnet/WinSock_Telnet.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <string>
+
+#pragma comment(lib, "Ws2_32.lib")
+#define DEFAULT_PORT "23"
+#define DEFAULT_BUFLEN 512
+
+int main(int argc, char **argv) {
+	WSADATA wsaData;
+	int iResult;
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+	struct addrinfo *result = NULL,
+					*ptr = NULL,
+					hints;
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	iResult = getaddrinfo("india.colorado.edu", "13", &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	ptr = result;
+	ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+	iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		closesocket(ConnectSocket);
+		ConnectSocket = INVALID_SOCKET;
+	}
+	freeaddrinfo(result);
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Unable to connect to server!\n");
+		WSACleanup();
+		return 1;
+	}
+	int recvbuflen = DEFAULT_BUFLEN;
+	const char* sendbuf = "r";
+	char recvbuf[DEFAULT_BUFLEN];
+	iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+	if (iResult == SOCKET_ERROR) {
+		printf("send faield %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+	printf("Bytes sent %ld\n", iResult);
+	iResult = shutdown(ConnectSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+	do {
+		iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+		if (iResult > 0) {
+			printf("Bytes received %d\n", iResult);
+			for (int i = 0; i < 512; i++) {
+				if (recvbuf[i-1] != '*') {
+					std::cout << recvbuf[i];
+				}
+				else {
+					std::cout << std::endl;
+					break;
+				}
+			}
+		}
+			
+		else if (iResult == 0) {
+			printf("Connetion closed\n");
+		}
+			
+		else {
+			printf("recv failed: %d\n", WSAGetLastError());
+		}	
+	} while (iResult > 0);
+	closesocket(ConnectSocket);
+	WSACleanup();
+	return 0;
+}
